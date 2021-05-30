23fdadad3f9d969d959f5ef35ed36a75b9bb08cf
ourylydibin <59970487+ourylydibin@users.noreply.github.com>
2021-05-30 16:33:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def search():
         try:
             #url_mongo = f"mongodb://localhost:27017/"
             url_mongo = "mongodb+srv://oury:touga@oury.p7kgd.mongodb.net/reviews_new?retryWrites=true&w=majority"
-            clien = pymongo.MongoClient(os.getenv(url_mongo), ssl= True, ssl_cert_reqs='CERT_NONE')
+            clien = pymongo.MongoClient(os.getenv(url_mongo), ssl= False, ssl_cert_reqs='CERT_NONE')
             dataBase = clien["reviews_new"]
             review = dataBase[searchString]
             xl = review.find({})
