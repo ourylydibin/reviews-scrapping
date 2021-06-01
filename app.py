b183ebaf7cd13262a7d99e0e0eaadd2799bb510d
ourylydibin <59970487+ourylydibin@users.noreply.github.com>
2021-05-31 22:45:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -23,53 +23,54 @@ def search():
         try:
             #url_mongo = f"mongodb://localhost:27017/"
             #, ssl=True, ssl_cert_reqs='CERT_NONE'
-            clien = pymongo.MongoClient(os.environ['MONGODB_URI'], ssl=True, ssl_cert_reqs='CERT_NONE')
-            dataBase = clien["reviews_new"]
-            review = dataBase[searchString]
-            xl = review.find({})
-            if xl.count() > 0:
-                return render_template("results_mine.html", result=xl)
-            else:
-                flipkart_url = "https://www.flipkart.com/search?q="+searchString
-                uClient = uReq(flipkart_url)
-                flipkartPage = uClient.read()
-                uClient.close()
-                flipkart_html = bs(flipkartPage, "html.parser")  # parsing the webpage as HTML
-                bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
-                del bigboxes[0:3]
-                box = bigboxes[0]
-                productlink = "https://flipkart.com"+box.div.div.div.a["href"]
-                page = requests.get(productlink)
-                beauty_page = bs(page.text, "html.parser")  # parsing the product page as HTML
-                comment_section = beauty_page.find_all("div", {"class": "_16PBlm"})
-                reviews = []
-                table = dataBase[searchString]
-                for rating_heading_comment_name in comment_section:
+            if os.environ.get('ENV')=='production':
+                clien = pymongo.MongoClient(os.environ.get('MONGODB_URI'), ssl=True, ssl_cert_reqs='CERT_NONE')
+                dataBase = clien["reviews_new"]
+                review = dataBase[searchString]
+                xl = review.find({})
+                if xl.count() > 0:
+                    return render_template("results_mine.html", result=xl)
+                else:
+                    flipkart_url = "https://www.flipkart.com/search?q="+searchString
+                    uClient = uReq(flipkart_url)
+                    flipkartPage = uClient.read()
+                    uClient.close()
+                    flipkart_html = bs(flipkartPage, "html.parser")  # parsing the webpage as HTML
+                    bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
+                    del bigboxes[0:3]
+                    box = bigboxes[0]
+                    productlink = "https://flipkart.com"+box.div.div.div.a["href"]
+                    page = requests.get(productlink)
+                    beauty_page = bs(page.text, "html.parser")  # parsing the product page as HTML
+                    comment_section = beauty_page.find_all("div", {"class": "_16PBlm"})
+                    reviews = []
+                    table = dataBase[searchString]
+                    for rating_heading_comment_name in comment_section:
 
-                    try:
-                        rating = rating_heading_comment_name.find_all("div", {"class": "_3LWZlK _1BLPMq"})[0].text
-                    except:
-                        rating = "No rating"
+                        try:
+                            rating = rating_heading_comment_name.find_all("div", {"class": "_3LWZlK _1BLPMq"})[0].text
+                        except:
+                            rating = "No rating"
 
-                    try:
-                        heading = rating_heading_comment_name.findAll("p", {"class": "_2-N8zT"})[0].text
-                    except:
-                        heading = "No heading"
+                        try:
+                            heading = rating_heading_comment_name.findAll("p", {"class": "_2-N8zT"})[0].text
+                        except:
+                            heading = "No heading"
 
-                    try:
-                        comment = rating_heading_comment_name.findAll("div", {"class": ""})[0].div.text
-                    except:
-                        comment = "No comment"
+                        try:
+                            comment = rating_heading_comment_name.findAll("div", {"class": ""})[0].div.text
+                        except:
+                            comment = "No comment"
 
-                    try:
-                        name = rating_heading_comment_name.findAll("p", {"class": "_2sc7ZR _2V5EHH"})[0].text
-                    except:
-                        name = "No name"
+                        try:
+                            name = rating_heading_comment_name.findAll("p", {"class": "_2sc7ZR _2V5EHH"})[0].text
+                        except:
+                            name = "No name"
 
-                    reviews_summary = {"product": searchString,"rating": rating,"heading": heading,"comment": comment,"name": name}
-                    x=table.insert_one(reviews_summary)
-                    reviews.append(reviews_summary)
-                return render_template("results_mine.html", result=reviews)
+                        reviews_summary = {"product": searchString,"rating": rating,"heading": heading,"comment": comment,"name": name}
+                        x=table.insert_one(reviews_summary)
+                        reviews.append(reviews_summary)
+                    return render_template("results_mine.html", result=reviews)
         except:
             return "something is wrong"
     else:
