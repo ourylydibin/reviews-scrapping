956307eca7b13dcdfaf17d97ea04986f6a90301c
ourylydibin <59970487+ourylydibin@users.noreply.github.com>
2021-06-01 00:38:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def search():
                     reviews.append(reviews_summary)
                 return render_template("results_mine.html", result=reviews)
         except:
-            return str(os.environ['MONGODB_URI'])
+            return type(os.environ['MONGODB_URI'])
     else:
         render_template("html_custom.html")
 if __name__ == "__main__":
